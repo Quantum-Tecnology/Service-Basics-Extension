518957daf09cdf80ec55dd74d00b4b2e62e5c155
Luis Gustavo Santarosa Pinto <bolota_xd@hotmail.com>
2024-08-26 21:53:02 -0300

    add system booting

--- src/BaseService.php
+++ src/BaseService.php
@@ -25,6 +25,9 @@ class BaseService
     protected bool $sync              = false;
     protected bool $qaTest            = false;
     protected $authUser;
+
+    protected string $model;
+
     protected ?Model $defaultModel;
 
     /**
@@ -35,6 +38,17 @@ class BaseService
     protected array $searchableColumns = [];
 
     protected array $searchableRelations = [];
+
+    public function __construct()
+    {
+        $this->bootIfNotBooted();
+
+        $this->initializeTraits();
+
+        if (!is_null($this->model)) {
+            $this->defaultModel = new $this->model();
+        }
+    }
 
     public function index(): LengthAwarePaginator|Collection
     {
@@ -76,7 +90,11 @@ class BaseService
             $this->unprocessableEntityException('Parameter search not enabled this route.');
         }
 
-        $query->orderby(request()->sortby ?? 'id', request()->sort ?? 'asc');
+        if ('random' === request()->sortby) {
+            $query->inRandomOrder();
+        } else {
+            $query->orderby(request()->sortby ?? 'id', request()->sort ?? 'asc');
+        }
 
         return $this->result($query);
     }
@@ -232,5 +250,119 @@ class BaseService
         $this->setData($data);
 
         return new Collection($segment ?? []);
+    }
+
+    /**
+     * The array of booted models.
+     *
+     * @var array
+     */
+    protected static $booted = [];
+
+    /**
+     * The array of trait initializers that will be called on each new instance.
+     *
+     * @var array
+     */
+    protected static $traitInitializers = [];
+
+    /**
+     * Check if the model needs to be booted and if so, do it.
+     *
+     * @return void
+     */
+    protected function bootIfNotBooted()
+    {
+        if (!isset(static::$booted[static::class])) {
+            static::$booted[static::class] = true;
+
+            static::booting();
+            static::boot();
+            static::booted();
+        }
+    }
+
+    /**
+     * Initialize any initializable traits on the model.
+     *
+     * @return void
+     */
+    protected function initializeTraits()
+    {
+        foreach (static::$traitInitializers[static::class] as $method) {
+            $this->{$method}();
+        }
+    }
+
+    /**
+     * Perform any actions required before the model boots.
+     *
+     * @return void
+     */
+    protected static function booting()
+    {
+    }
+
+    /**
+     * Bootstrap the model and its traits.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        static::bootTraits();
+    }
+
+    /**
+     * Perform any actions required after the model boots.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+    }
+
+    /**
+     * Boot all of the bootable traits on the model.
+     *
+     * @return void
+     */
+    protected static function bootTraits()
+    {
+        $class = static::class;
+
+        $booted = [];
+
+        static::$traitInitializers[$class] = [];
+
+        foreach (class_uses_recursive($class) as $trait) {
+            $method = 'boot'.class_basename($trait);
+
+            if (method_exists($class, $method) && !in_array($method, $booted)) {
+                forward_static_call([$class, $method]);
+
+                $booted[] = $method;
+            }
+
+            if (method_exists($class, $method = 'initialize'.class_basename($trait))) {
+                static::$traitInitializers[$class][] = $method;
+
+                static::$traitInitializers[$class] = array_unique(
+                    static::$traitInitializers[$class]
+                );
+            }
+        }
+    }
+
+    /**
+     * When a model is being unserialized, check if it needs to be booted.
+     *
+     * @return void
+     */
+    public function __wakeup()
+    {
+        $this->bootIfNotBooted();
+
+        $this->initializeTraits();
     }
 }
