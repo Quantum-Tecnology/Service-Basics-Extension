717ee46290418141607a8f74b52a71117b7800e8
Gustavo Santarosa <gustavo-computacao@hotmail.com>
2025-03-16 20:18:40 -0300

    Created the scopes trait to perform automatic searches according to user requests

---
+++ src/Traits/FilterScopesTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace QuantumTecnology\ServiceBasicsExtension\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
+
+trait FilterScopesTrait
+{
+    protected ?array $scopes = [];
+
+    public function addScopesFilter(?array $scopes = []): self
+    {
+        if (blank($this->getScopes())) {
+            $this->setScopes($scopes);
+        }
+
+        if (!$this->runningInConsole) {
+            $this->setScopes(request('filter', []));
+        }
+
+        $this->getScopes()
+            ->each(function ($scope, $key) {
+                $nameFilter = str("by_{$key}")->camel()->toString();
+                $nameScoped = str("scope_by_{$key}")->camel()->toString();
+                $dataFilter = collect(explode('|', $filter ?? ''))
+                    ->filter(fn ($item) => filled($item))
+                    ->toArray();
+
+                $this->defaultQuery()->$nameFilter(array_values($dataFilter));
+            });
+
+        return $this;
+    }
+
+    public function setScopes(?array $scopes): self
+    {
+        if (App::runningInConsole()) {
+            $this->runningInConsole = true;
+        }
+
+        $this->scopes = $scopes;
+
+        return $this;
+    }
+
+    public function getScopes(): Collection
+    {
+        return collect($this->scopes);
+    }
+}
