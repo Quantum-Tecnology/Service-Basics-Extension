a868468dfdd2b8dcbdc33a56b25f29bb941d258d
Gustavo Santarosa <gustavo-computacao@hotmail.com>
2025-03-16 20:19:34 -0300

    Created the Search trait to better organize its logic

--- src/Traits/FilterSearchTrait.php
+++ src/Traits/FilterSearchTrait.php
@@ -3,20 +3,34 @@
 namespace QuantumTecnology\ServiceBasicsExtension\Traits;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\App;
 
 trait FilterSearchTrait
 {
-    public function addSearchFilter(
-        ?string $search = null,
-    ): void {
+    protected ?string $search = '';
+
+    protected array $searchableColumns = [];
+
+    protected array $searchableRelations = [];
+
+    public function addSearchFilter(?string $search = ''): self
+    {
+        if (blank($this->getSearch())) {
+            $this->setSearch($search);
+        }
+
+        if (!$this->runningInConsole) {
+            $this->setSearch(request('search', $this->getSearch()));
+        }
+
         abort_if(
-            !empty($search) && (count($this->searchableColumns) > 0 || count($this->searchableRelations) > 0),
+            $this->isSearchable(),
             Response::HTTP_UNPROCESSABLE_ENTITY,
             __('Parameter search not enabled this route.'),
         );
 
-        $this->query->where(function ($subQuery) use ($search) {
-            $searchString = str_replace(' ', '%', trim($search));
+        $this->defaultQuery()->where(function ($subQuery) {
+            $searchString = str_replace(' ', '%', trim($this->getSearch()));
 
             $subQuery->where(function ($query) use ($searchString) {
                 foreach ($this->searchableColumns as $column) {
@@ -34,5 +48,28 @@ trait FilterSearchTrait
                 });
             }
         });
+
+        return $this;
+    }
+
+    private function isSearchable(): bool
+    {
+        return !blank($this->getSearch()) && !(count($this->searchableColumns) > 0 || count($this->searchableRelations) > 0);
+    }
+
+    public function setSearch(string $search): self
+    {
+        if (App::runningInConsole()) {
+            $this->runningInConsole = true;
+        }
+
+        $this->search = $search;
+
+        return $this;
+    }
+
+    public function getSearch(): string
+    {
+        return $this->search;
     }
 }
